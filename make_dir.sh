b7bd565d4feb7dca2845500c9f8cd51a1111cda2
bosheng0701 <sgps8871@gmail.com>
2021-07-28 11:49:46 +0000

    20210728

---
+++ make_dir.sh
@@ -0,0 +1,70 @@
+mkdir ispass
+mkdir pannotia
+mkdir parboil
+mkdir polybench
+mkdir rodinia 
+mkdir sdk
+
+
+# ispass
+cd ispass 
+mkdir bfs
+cd ..
+#
+
+# pannotia
+cd pannotia
+cd ..
+#
+
+# parboil
+cd parboil
+mkdir parboil-spmv
+cd ..
+#
+
+# polybench
+cd polybench
+mkdir 2DC
+mkdir axat
+mkdir mvt
+cd ..
+#
+
+# rodinia
+cd rodinia
+mkdir 3.1
+cd 3.1
+mkdir cuda
+cd cuda
+mkdir b+tree
+mkdir backprop
+mkdir bfs
+mkdir cfd
+mkdir dwt2d
+mkdir gaussian
+mkdir heartwall
+mkdir hotspot
+mkdir hotspot3D
+mkdir huffman
+mkdir hybridsort
+mkdir kmeans
+mkdir lavaMD
+mkdir leukocyte
+mkdir lud 
+mkdir myocyte
+mkdir nn
+mkdir nw
+mkdir particlefiliter
+mkdir pathfinder
+mkdir srad
+mkdir streamcluster
+cd 
+cd benchmark_run
+#
+
+# sdk 
+cd sdk 
+mkdir dct
+cd ..
+#
