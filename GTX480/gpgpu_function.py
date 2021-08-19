5bd94e3517b42018288899b757f1429cb86bafa4
bosheng0701 <sgps8871@gmail.com>
2021-08-19 10:59:24 +0000

    0819_status_collect

--- GTX480/gpgpu_function.py
+++ GTX480/gpgpu_function.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def convert_cta_total():
+    f=open('total_cta.txt','r')
+    b=[]
+    shader_total_cta=np.zeros((15),dtype=np.int32)
+    for j in f:
+        b.append(j.strip().split(','))
+    for i,j in b:
+        shader_total_cta[int(i)]=j
+    f.close()
+    f=open('total_cta.txt','w')
+    for i in range(15):
+        f.write(str(i+1)+','+str(shader_total_cta[i])+'\n')   
+    f.close()
+    
 def function_cta_retire_time_transform():
     f =open('cta_retire.txt','r')
     a=[]
@@ -13,15 +27,21 @@ def function_cta_retire_time_transform():
     ffp.close()
     #-----------------------------------
     fp=open('cta_retire_mean.txt','w')
-    
+    percent_cta=[]
+    for i in range(15):
+        percent_cta.append([])
     shader_total_rate=np.zeros((15),dtype=np.int32)
     for i in a:
         shader_total_rate[int(i[0])]=int(i[4])
+        percent_cta[int(i[0])].append(int(i[3]))
     for i,j in b:
         shader_total_rate[int(i)-1]=shader_total_rate[int(i)-1]/int(j)
+    fp.write('sid  mean  75%     50%    25%\n')
     for i in range(15):
-        fp.write(str(i+1)+' '+str(shader_total_rate[i])+'\n')    
+        fp.write(str(i+1)+'    '+str(shader_total_rate[i])+'   '+str(np.percentile(percent_cta[i],75))+'  '+str(np.percentile(percent_cta[i],50))+'  '+str(np.percentile(percent_cta[i],25))+'\n')    
     fp.close()
+    
+
     #----------------------------------------------
     ffp=open('top10_cta_retire.txt','w')
     shader_max=np.zeros((15,10),dtype=np.int32)
@@ -47,14 +67,19 @@ def function_unit_time_transform(total_cycle):
     f.close()
     #----------------------------------------------------------
     fp=open('total_rate_FU.txt','w')
+    fu_percent=[]
+    for i in range(15):
+        fu_percent.append([])
     shader_total_rate=np.zeros((15),dtype=np.float32)
     for i in a:
         shader_total_rate[int(i[0])]=int(i[4])/total_cycle
+        fu_percent[int(i[0])].append(int(i[3]))
+    fp.write('sid  busy_rate  75%     50%    25%\n')
     for i in range(15):
-        fp.write(str(i+1)+' '+str(round(shader_total_rate[i]*100,2))+'%\n')
+        fp.write(str(i+1)+' '+str(round(shader_total_rate[i]*100,2))+'%'+'   '+str(np.percentile(fu_percent[i],75))+'  '+str(np.percentile(fu_percent[i],50))+'  '+str(np.percentile(fu_percent[i],25))+'\n')
     fp.close()
     #--------------------------------------------------------
-    ffp=open('top10.txt_FU','w')
+    ffp=open('top10_FU.txt','w')
     shader_max=np.zeros((15,10),dtype=np.int32)
     count = np.zeros((15),dtype=np.int32)
     for i in a:
--- GTX480/main.py
+++ GTX480/main.py
@@ -1,5 +1,6 @@
 import gpgpu_function as ft
 
 total_cycle=int(input('please key in the benchmark total cycle: ')) # please update the benchmark total cycle
+# ft.convert_cta_total()
 ft.function_cta_retire_time_transform()
 ft.function_unit_time_transform(total_cycle)
