b6f8b471febea2bf108864a193eea89738853b8c
bosheng0701 <sgps8871@gmail.com>
2022-05-19 15:02:03 +0000

    220519

--- cache_sensitive.sh
+++ cache_sensitive.sh
@@ -28,7 +28,7 @@
  cd /root/benchmark_run/ispass/bfs
  SECONDS=0
  echo  "BFS run" |tee -a time.txt
- /root/gpgpu-sim_simulations/benchmarks/bin/4.2/release/ispass-2009-BFS /root/gpgpu-sim_simulations/benchmarks/data_dirs/cuda/rodinia/3.1/bfs-rodinia-3.1/data/graph65536.txt >BFS65536.txt
+ /root/gpgpu-sim_simulations/benchmarks/bin/4.2/release/ispass-2009-BFS /root/gpgpu-sim_simulations/benchmarks/data_dirs/cuda/ispass-2009/ispass-2009-BFS/data/graph65536.txt >BFS65536.txt
  duration=$SECONDS
  echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."|tee -a time.txt
  rm _cuobjdump*
